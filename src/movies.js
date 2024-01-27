ebfa107e4967860fe298eda7b5a91351773e9282
simonpizevski <78857703+simonpizevski@users.noreply.github.com>
2024-01-27 02:21:34 +0100

    shows movies on home

--- src/movies.js
+++ src/movies.js
@@ -5,21 +5,11 @@ const API  = 'https://plankton-app-xhkom.ondigitalocean.app/api';
 export async function loadMovies() {
     const res = await fetch(API + '/movies');
     const payload = await res.json();
-    const modifiedArray = payload.data.map((obj) => {
-        return {
-            id: obj.id,
-            ...obj.attributes,
-        };
-    });
-    return modifiedArray;
+    return payload.data;
 } 
 
 export async function loadMovie(id) {
     const res = await fetch(API + '/movies/' + id);
     const payload = await res.json();
-    
-    return {
-        id: payload.data.id, 
-        ...payload.data.attributes,
-    };
+    return payload.data;
 }
