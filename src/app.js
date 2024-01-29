0cd2710d89020edac76eac9027496f9c7be5faa2
simonpizevski <78857703+simonpizevski@users.noreply.github.com>
2024-01-29 15:44:34 +0100

    added header

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import { loadMovie, loadMovies } from "./movies.js";
+import express from 'express';
+import { engine } from 'express-handlebars';
+import { loadMovie, loadMovies } from './movies.js';
 
 const app = express();
 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./templates");
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
 
-app.get("/", async (req, res) => {
-    const movies = await loadMovies();
-    res.render("home", { movies });
+app.get('/', async (req, res) => {
+  res.render('home');
 });
 
-app.get("/movies/:id", async (req, res) => {
-    const movie = await loadMovie(req.params.id);
-    res.render("movie", { movie });
+app.get('/movies', async (req, res) => {
+  const movies = await loadMovies();
+  res.render('movies', { movies });
 });
 
-app.use("/static", express.static("./static"));
+app.get('/movies/:id', async (req, res) => {
+  const movie = await loadMovie(req.params.id);
+  res.render('movie', { movie });
+});
+
+app.get('/', async (req, res) => {
+  res.render('contact', { contact });
+});
+
+app.use('/static', express.static('./static'));
 
 export default app;
